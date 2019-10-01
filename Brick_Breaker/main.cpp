10bfcaf7f277a092f98702ec703afdc72438d222
krompac <tomijehrvat@gmail.com>
2019-10-01 12:47:27 +0200

    Mala izmjena main.cpp-a

--- Brick_Breaker/main.cpp
+++ Brick_Breaker/main.cpp
@@ -5,10 +5,7 @@
 #include "raylib.h"
 #include "Gameplay_Window.h"
 #include "Menu_Window.h"
-#include "Current_Window.h"
 #include "Button.h"
-
-Current_Window window = Lobby;
 
 Window *active = nullptr;
 Gameplay_Window *gameplay = nullptr;
@@ -31,15 +28,11 @@ int main()
 	auto toGame = [] { active = gameplay; DisableCursor(); };
 	auto toMenu = [] {active = menu; EnableCursor(); };
 	auto quitGame = [] {quit = true; };
-	auto resume = [] {};
+	auto restart = [] {/*TO DO:*/};
+	auto resume = [] { /*TO DO:*/};
 	
 	gameplay = new Gameplay_Window(toMenu);
-	menu = new Menu_Window(toGame, toMenu, resume, quitGame);
-
-	Color color = RED;
-	const char *text = "BUTTON";
-	auto button = new Button(400, 400, 100, 50, color, text, sleep);
-
+	menu = new Menu_Window(toGame, resume, restart, quitGame);
 	active = menu;
 
 	while (!WindowShouldClose() && !quit)
@@ -58,7 +51,6 @@ int main()
 
 	CloseWindow();
 
-	delete button;
 	delete menu;
 	delete gameplay;
 
